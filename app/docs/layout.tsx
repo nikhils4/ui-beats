6219990b3e253c75420e69fe6527da812dce5758
Nikhil Singh <nikhilsingh498@gmail.com>
2024-08-09 05:33:32 -0400

    Added the right column

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -1,10 +1,11 @@
 import type { Metadata } from "next";
 import { ReactNode } from "react";
 import { SideNav } from "@/components/website/side-nav";
+import { Bug, Edit3 } from "lucide-react";
 
 export const metadata: Metadata = {
   title: "ui/beats",
-  description: "Develop effortless stunning UI with reusable components",
+  description: "Develop stunning UI effortlessly with reusable components",
 };
 
 export default function RootLayout({
@@ -24,7 +25,32 @@ export default function RootLayout({
           </div>
         </main>
         <div className="hidden xl:block w-64 px-8 py-8">
-          <div className="text-sm">The very right column</div>
+          <div className="space-y-2">
+            <ul className="m-0 list-none">
+              <li className="mt-0 pt-2">
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="inline-flex items-center text-sm text-muted-foreground hover:text-foreground transition-colors"
+                  href="https://github.com/nikhils4/ui-beats"
+                >
+                  <Bug className="h-4 w-4 mr-2" />
+                  Report an issue
+                </a>
+              </li>
+              <li className="mt-0 pt-2">
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="inline-flex items-center text-sm text-muted-foreground hover:text-foreground transition-colors"
+                  href="https://github.com/nikhils4/ui-beats"
+                >
+                  <Edit3 className="h-4 w-4 mr-2" />
+                  Request a feature
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>

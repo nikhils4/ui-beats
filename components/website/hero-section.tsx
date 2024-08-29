a4ad559ce2a97a7784be76ab5c4058cf7f3320f3
Nikhil Singh <nikhilsingh498@gmail.com>
2024-08-29 17:38:19 -0400

    Updated pill

--- components/website/hero-section.tsx
+++ components/website/hero-section.tsx
@@ -6,7 +6,7 @@ import { Badge } from "@/components/ui/badge";
 import { motion } from "framer-motion";
 
 export const HeroSection = () => {
-  let libraryName = "Gravity Text Swap Component";
+  let libraryName = "Glowing Card";
 
   const containerVariants = {
     hidden: { opacity: 0 },
@@ -39,7 +39,7 @@ export const HeroSection = () => {
     >
       <div className="flex flex-col items-start gap-6 px-7 pb-8 text-center md:items-center md:px-10">
         <motion.div variants={itemVariants}>
-          <Link href="/docs/modern-animation/gravity-text-swap">
+          <Link href="/docs/modern-animation/glowing-card">
             <div className="group relative mx-auto flex max-w-fit flex-row items-center justify-center rounded-2xl bg-white/40 px-4 py-1.5 text-sm font-medium shadow-[inset_0_-8px_10px_#8fdfff1f] backdrop-blur-sm transition-shadow duration-500 ease-out hover:shadow-[inset_0_-5px_10px_#8fdfff3f] dark:bg-black/40">
               <div className="absolute inset-0 block h-full w-full bg-gradient-to-r from-[#ff7a40]/50 via-[#ff4040]/50 to-[#ff7a40]/50 bg-[length:400%_100%] animate-gradient-x p-[1px] ![mask-composite:subtract] [border-radius:inherit] [mask:linear-gradient(#fff_0_0)_content-box,linear-gradient(#fff_0_0)] dark:hidden"></div>
               <div className="absolute inset-0 block h-full w-full bg-gradient-to-r from-[#4050ff]/50 via-[#40ff7a]/50 to-[#4050ff]/50 bg-[length:400%_100%] animate-gradient-x p-[1px] ![mask-composite:subtract] [border-radius:inherit] [mask:linear-gradient(#fff_0_0)_content-box,linear-gradient(#fff_0_0)] dark:block"></div>
@@ -61,17 +61,26 @@ export const HeroSection = () => {
             </div>
           </Link>
         </motion.div>
-        <motion.div variants={itemVariants} className="relative flex flex-col gap-10 md:items-center lg:flex-row mt-6">
+        <motion.div
+          variants={itemVariants}
+          className="relative flex flex-col gap-10 md:items-center lg:flex-row mt-6"
+        >
           <h1 className="text-black dark:text-white relative mx-0 max-w-[43.5rem] pt-5 md:mx-auto md:px-4 md:py-2 text-left tracking-tighter text-balance md:text-center font-semibold md:text-7xl lg:text-7xl sm:text-7xl text-5xl">
             Supercharge your UI
           </h1>
         </motion.div>
-        <motion.p variants={itemVariants} className="max-w-xl mt-6 mb-6 text-balance text-left text-base tracking-tight text-black dark:font-medium dark:text-white md:text-center md:text-lg ">
+        <motion.p
+          variants={itemVariants}
+          className="max-w-xl mt-6 mb-6 text-balance text-left text-base tracking-tight text-black dark:font-medium dark:text-white md:text-center md:text-lg "
+        >
           Level up your UI development with reusable components from ui/beats,
           crafted with <b>React</b>, <b>Typescript</b>, <b>Tailwind CSS</b>, and
           <b> Framer Motion</b>.
         </motion.p>
-        <motion.div variants={itemVariants} className="flex flex-col md:flex-row">
+        <motion.div
+          variants={itemVariants}
+          className="flex flex-col md:flex-row"
+        >
           <Link
             rel="noopener noreferrer"
             href="/docs/getting-started"

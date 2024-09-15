9e2c809e1aa4db7df4dd9d2ad08c90fac3b5835b
Nikhil Singh <nikhilsingh498@gmail.com>
2024-09-15 01:21:33 -0400

    Removed console.log

--- app/api/rss.xml/route.ts
+++ app/api/rss.xml/route.ts
@@ -3,7 +3,6 @@ import { NextResponse } from 'next/server'
 
 export async function GET() {
   const feed = await generateRssFeed()
-  console.log(feed)
   return new NextResponse(feed.rss2(), {
     headers: { 'Content-Type': 'application/xml' },
   })

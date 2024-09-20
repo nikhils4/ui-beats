66dca2f93088f6b31b3db13ba7e428c4b6dfa73e
Nikhil Singh <nikhilsingh498@gmail.com>
2024-09-19 21:56:46 -0400

    fix: Update the description of the button

--- content/docs/components/subscribe-button.content.tsx
+++ content/docs/components/subscribe-button.content.tsx
@@ -17,11 +17,12 @@ const SubscribeButtonContent: ComponentConfigType = {
   ],
   title: "Subscribe Button",
   description:
-    "The SubscribeButton component is an interactive button designed for subscription actions, featuring customizable styles and animations.",
+    "The SubscribeButton component is an interactive button designed for subscription actions (more like Youtube Subscribe Button Animation), featuring customizable styles and animations.",
   usageCode: `${getAnimationPreview("subscribe-button", 1, true)}`,
   installation: [
     {
-      description: "If you want to use dynamic themes, add the following to your project (optional)",
+      description:
+        "If you want to use dynamic themes, add the following to your project (optional)",
       code: `import { useTheme } from "next-themes";
 
 const YourComponent = () => {
